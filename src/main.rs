06be11237c8667a29c55b72bf9e0aedaed655963
Marusiella <67511608+Marusiella@users.noreply.github.com>
2022-04-23 16:14:39 +0200

    It's working!

---
+++ src/main.rs
@@ -0,0 +1,162 @@
+use std::time;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+#[derive(Debug)]
+struct ToMine {
+    from: String,
+    to: String,
+    difficulty: u128,
+}
+impl ToMine {
+    fn new(msg: String) -> Self {
+        let data = msg.split(",").collect::<Vec<&str>>();
+        println!("{:?} data z msg", data);
+        ToMine {
+            from: data[0].to_owned(),
+            to: data[1].to_owned(),
+            difficulty: data[2]
+                .trim_matches('\u{0}')
+                .trim_matches('\n')
+                .trim()
+                .parse::<u128>()
+                .unwrap(),
+        }
+    }
+}
+#[tokio::main]
+async fn main() {
+    let mut tcpstream = TcpStream::connect("51.159.175.20:6043").await.unwrap();
+    let mut buf = vec![0; 1024];
+    tcpstream.read(&mut buf).await.unwrap();
+    println!("Server version is: {}", String::from_utf8_lossy(&buf));
+    tcpstream.write(b"JOB,Mareczekk,EXTRIME,test").await.unwrap();
+    tcpstream.read(&mut buf).await.unwrap();
+    let hash = String::from_utf8_lossy(&buf).to_string().trim().to_string();
+    let to_mine = ToMine::new(hash);
+    // print!("{:?}", to_mine);
+    use sha1::{Digest, Sha1};
+    let mut hasher = Sha1::new();
+    // use std::time::Instant;
+
+    let timel = time::SystemTime::now()
+        .duration_since(time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs_f64();
+
+    hasher.update(to_mine.from.as_bytes());
+    // print!("{:x}", hasher.finalize());
+    let mut thisNumber = 0;
+    for result in 1..=((100 * to_mine.difficulty) + 1) {
+        let mut hasher = hasher.clone();
+        hasher.update(result.to_string().as_bytes());
+        if to_mine.to == format!("{:x}", hasher.clone().finalize()) {
+            println!("Result = {}", result);
+            println!("Wydobyto na {}%", (result/((100 * to_mine.difficulty) + 1)) * 100);
+            thisNumber = result;
+            break;
+        }
+        // println!("{} |  {}", format!("{:x}", hasher.clone().finalize()), to_mine.to);
+    }
+    let time_of_doing = time::SystemTime::now()
+        .duration_since(time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs_f64()
+        - timel;
+    // println!("{}", time_of_doing);
+    println!(
+        "hashrate is {} kH/s",
+        (thisNumber as f64 / time_of_doing) / 1000.0
+    );
+    // before to unix time
+    // println!(
+    //     "{}",
+    //     time::SystemTime::now()
+    //         .duration_since(time::UNIX_EPOCH)
+    //         .unwrap()
+    //         .as_secs_f64()
+    // );
+    // wysłano 1248903,934366.4534479105,Minimal_PC_Miner
+    println!("Wysłano: {}",format!("{},{},RUST,test", thisNumber, thisNumber as f64 / time_of_doing));
+    tcpstream
+        .write(format!("{},{},RUST,test", thisNumber, thisNumber as f64 / time_of_doing).as_bytes())
+        .await
+        .unwrap();
+    buf.clear();
+    tcpstream.read(&mut buf).await.unwrap();
+    println!("z {}", String::from_utf8_lossy(&buf));
+
+    {
+        let mut buf = vec![0; 1024];
+        tcpstream.write(b"JOB,Mareczekk,MEDIUM,test").await.unwrap();
+        tcpstream.read(&mut buf).await.unwrap();
+        while String::from_utf8_lossy(&buf).to_string().trim() == "" {
+            tcpstream.read(&mut buf).await.unwrap();
+        }
+
+        let hash = String::from_utf8_lossy(&buf).to_string().trim().to_string();
+        let converted = hash.trim_matches('\u{0}')
+        .trim_matches('\n')
+        .trim();
+        println!("{}", converted);
+        // let to_mine = ToMine::new(hash);
+        // // print!("{:?}", to_mine);
+        // use sha1::{Digest, Sha1};
+        // let mut hasher = Sha1::new();
+        // // use std::time::Instant;
+
+        // let timel = time::SystemTime::now()
+        //     .duration_since(time::UNIX_EPOCH)
+        //     .unwrap()
+        //     .as_secs_f64();
+
+        // hasher.update(to_mine.from.as_bytes());
+        // let mut thisNumber = 0;
+        // for result in 0..((100 * to_mine.difficulty) + 1) {
+        //     let mut hasher = hasher.clone();
+        //     hasher.update(result.to_string().as_bytes());
+        //     if to_mine.to == format!("{:x}", hasher.clone().finalize()) {
+        //         println!("{}", result);
+        //         thisNumber = result;
+        //         break;
+        //     }
+        //     // println!("{} |  {}", format!("{:x}", hasher.clone().finalize()), to_mine.to);
+        // }
+        // let time_of_doing = time::SystemTime::now()
+        //     .duration_since(time::UNIX_EPOCH)
+        //     .unwrap()
+        //     .as_secs_f64()
+        //     - timel;
+        // println!("{}", time_of_doing);
+        // println!(
+        //     "hashrate is {} kH/s",
+        //     (thisNumber as f64 / time_of_doing) / 1000.0
+        // );
+        // // before to unix time
+        // println!(
+        //     "{}",
+        //     time::SystemTime::now()
+        //         .duration_since(time::UNIX_EPOCH)
+        //         .unwrap()
+        //         .as_secs_f64()
+        // );
+        // tcpstream
+        //     .write(format!("{},{},RUST", thisNumber, thisNumber as f64 / time_of_doing).as_bytes())
+        //     .await
+        //     .unwrap();
+        // buf.clear();
+        // tcpstream.read(&mut buf).await.unwrap();
+        // println!("z {}", String::from_utf8_lossy(&buf));
+    }
+
+}
+// use std::time;
+// fn main() {
+//     // print current time as python as well
+//     // time.time()
+//     let timees = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs_f64();
+//     println!("1.5700244903564453");
+//     std::thread::sleep(time::Duration::from_secs(2));
+//     println!("{}", time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs_f64() - timees);
+// }
